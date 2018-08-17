05a86964b432972cac92a012e6e696600d1ec85a
Yumeng Liu <cindyliu2016@gmail.com>
2018-08-16 23:11:54 -0700

    Initial Check-in

---
+++ src/main/java/com/ylym/valuelogger/App.java
@@ -0,0 +1,13 @@
+package com.ylym.valuelogger;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/ylym/valuelogger/AppTest.java
@@ -0,0 +1,20 @@
+package com.ylym.valuelogger;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
